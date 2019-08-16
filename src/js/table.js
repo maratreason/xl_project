f68e15c0535f25400f56e0cd4d37d7416231fce3
Marat Zainullin <maratreason@mail.ru>
2019-08-16 21:15:19 +0300

    Try to create resize

--- src/js/table.js
+++ src/js/table.js
@@ -1,27 +1,41 @@
 
 window.onload = function(e) {
-  const button = document.querySelector('.panel-btn');
+  // const button = document.querySelector('.panel-btn');
+  const div = document.querySelector('.table');
 
   function createTable() {
-    const tbody = document.querySelector('.table-body__tr');
+    const table = document.createElement('table');
+    const tableBody = document.createElement('tbody');
+    const tbodyTr = document.createElement('tr');
+    // const cellInput = document.querySelector('.panel-add__cell').value;
+    // const columnInput = document.querySelector('.panel-add__column').value;
+    const cellInput = 5;
+    const columnInput = 5;
 
-    const cellInput = document.querySelector('.panel-add__cell').value;
-    const columnInput = document.querySelector('.panel-add__column').value;
-    console.log(cellInput);
-    console.log(columnInput);
+    tableBody.classList.add('.table-body');
+    tbodyTr.classList.add('.table-body__tr');
 
-    tbody.innerHTML = '';
+    tableBody.appendChild(tbodyTr);
+    table.appendChild(tableBody);
+    div.appendChild(table);
+
+    tbodyTr.innerHTML = '';
     
     for (let row = 0; row < columnInput; row++) {
       let tr = document.createElement('tr');
       for (let cell = 0; cell < cellInput; cell++) {
         let td = document.createElement('td');
         td.textContent = 'Some';
+        td.classList.add('.newTd');
         tr.appendChild(td);
       }
-      tbody.appendChild(tr);
+      tbodyTr.appendChild(tr);
     }
   }
 
-  button.addEventListener('click', createTable);
+  // button.addEventListener('click', createTable);
+  createTable();
+
 }
+
+  

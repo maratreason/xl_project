08683911245c1aecf39f75e1cd0b3e3034c25909
Marat Zainullin <maratreason@mail.ru>
2019-08-17 06:46:13 +0300

    add event to parent element of table

--- src/js/table.js
+++ src/js/table.js
@@ -2,15 +2,16 @@
 window.onload = function(e) {
   // const button = document.querySelector('.panel-btn');
   const div = document.querySelector('.table');
+  let cellInput = 5;
+  let columnInput = 5;
+  let table = document.createElement('table');
+  let tableBody = document.createElement('tbody');
 
-  function createTable() {
-    const table = document.createElement('table');
-    const tableBody = document.createElement('tbody');
+  function createTable(cellInp = 1, columnInp = 1) {
+    
     const tbodyTr = document.createElement('tr');
     // const cellInput = document.querySelector('.panel-add__cell').value;
     // const columnInput = document.querySelector('.panel-add__column').value;
-    const cellInput = 5;
-    const columnInput = 5;
 
     tableBody.classList.add('.table-body');
     tbodyTr.classList.add('.table-body__tr');
@@ -21,9 +22,9 @@ window.onload = function(e) {
 
     tbodyTr.innerHTML = '';
     
-    for (let row = 0; row < columnInput; row++) {
+    for (let row = 0; row < columnInp; row++) {
       let tr = document.createElement('tr');
-      for (let cell = 0; cell < cellInput; cell++) {
+      for (let cell = 0; cell < cellInp; cell++) {
         let td = document.createElement('td');
         td.textContent = 'Some';
         td.classList.add('.newTd');
@@ -33,8 +34,35 @@ window.onload = function(e) {
     }
   }
 
+  function removeTable() {
+    div.innerHTML = '';
+  }
+
+  function resize() {
+    let tr = document.querySelector('tr');
+    tr.addEventListener('click', function(e) {
+      console.log(e);
+      console.log(e.offsetX);
+      console.log(e.offsetY);
+
+      let offsetX = parseInt(e.offsetX);
+      let offsetY = parseInt(e.offsetY);
+
+      if (offsetX > 100) {
+        removeTable();
+        console.log(e.target.style.width);
+        cellInput++;
+        columnInput++;
+        createTable(cellInput, columnInput);
+      } else {
+        createTable();
+      }
+    });
+  }
+
   // button.addEventListener('click', createTable);
   createTable();
+  resize();
 
 }
 
